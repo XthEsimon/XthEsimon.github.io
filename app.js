db710e60dc2605f7cde33ebeb6a71ab826e987cf
XthEsimon <nomis-simon@gmx.at>
2016-11-27 14:57:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 2448,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'RotMG-BTC-Dice',
+  app_name: 'Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdXHw0UAAAAANjmTjDLBCnGxnfX16QHLG9BRz44',  // <----- EDIT ME!
